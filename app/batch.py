8b3f515261720c3e56f19f5e4b0e5e9c85d2e0ba
apecr <alberto.eyo@gmail.com>
2022-05-18 18:54:02 +0200

    feat()
    Allocate order_lines and save them in the batch

--- app/batch.py
+++ app/batch.py
@@ -1,5 +1,6 @@
 import dataclasses
 import datetime
+from functools import reduce
 
 from app.order_line import OrderLine
 
@@ -8,22 +9,39 @@ from app.order_line import OrderLine
 class Batch:
     reference: str
     sku: str
-    quantity: int
+    original_quantity: int
     eta: datetime.date
 
+    _allocated_order_lines: dict = dataclasses.field(default_factory=lambda: {})
+
     def allocate(self, order_line: OrderLine):
-        if self.quantity < order_line.quantity:
-            raise AllocateException()
-        self.quantity -= order_line.quantity
+        if not self._is_order_allocated(order_line) and self._is_same_product(order_line):
+            if self.original_quantity < order_line.quantity:
+                raise AllocateException()
+            # self.original_quantity -= order_line.quantity
+            self._allocated_order_lines[order_line.order_reference] = order_line
 
     @property
     def available_quantity(self):
-        return self.quantity
+        def __reduce_quantity(acc, line):
+            return acc + line.quantity
+        consumed_quantity = reduce(__reduce_quantity, self._allocated_order_lines.values(), 0)
+        return self.original_quantity - consumed_quantity
 
     def can_allocate(self, order_line):
-        if self.sku == order_line.sku:
-            return self.quantity >= order_line.quantity
+        if order_line.sku == self.sku:
+            return self.available_quantity >= order_line.quantity
         return False
+
+    def _is_order_allocated(self, order_line):
+        return order_line.order_reference in self._allocated_order_lines
+
+    def _is_same_product(self, order_line):
+        return order_line.sku == self.sku
+
+    def deallocate(self, order_line):
+        if self._is_order_allocated(order_line):
+            del self._allocated_order_lines[order_line.order_reference]
 
 
 class AllocateException(Exception):
