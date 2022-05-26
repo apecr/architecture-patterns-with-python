8cbad669c726c3b3c2dc6c9d4843e228def72215
apecr <alberto.eyo@gmail.com>
2022-05-26 13:50:35 +0200

    feat(app/repository.py)
    Abstraction for the repository

---
+++ app/repository.py
@@ -0,0 +1,14 @@
+import abc
+
+from app.batch import Batch
+
+
+class AbstractRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def add(self, batch: Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> Batch:
+        raise NotImplementedError
